12a28d0f8f2dda7b7c0b5acababba77cecb9a4fc
lex <sunsan05@gmail.com>
2025-02-16 12:29:29 +0800

    Fix Package SSRP->PassWall

--- SCRIPTS/R5S/r5s-part1.sh
+++ SCRIPTS/R5S/r5s-part1.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #
 # https://github.com/HEXtoDEC/ActionWRT
-# File name: imm-part1.sh
+# File name: r5s-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
 # Add SSRP feed source
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+
+# Add PassWall feeds
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
--- SCRIPTS/X86/x86-part1.sh
+++ SCRIPTS/X86/x86-part1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 # https://github.com/HEXtoDEC/ActionWRT
-# File name: lede-part1.sh
+# File name: x86-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
-# Add PassWall feeds
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
 # Add SSRP feed source
 # sed -i "/helloworld/d" "feeds.conf.default"
 # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+
+# Add PassWall feeds
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
